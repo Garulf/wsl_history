f00b42c19c70a13f55e32f3ea77c5e025358c8cb
Garulf <535299+Garulf@users.noreply.github.com>
2021-09-06 23:02:36 -0400

    init

---
+++ plugin/wsl_history.py
@@ -0,0 +1,12 @@
+from flox import Flox
+
+class WslHistory(Flox):
+
+    def query(self, query):
+        pass
+
+    def context(self, data):
+        pass
+
+if __name__ == "__main__":
+    WslHistory()
